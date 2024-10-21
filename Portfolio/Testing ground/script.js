4626e66c25146e500a82da604fa1b16cadb10524
Jonathan Gian <jongian90@gmail.com>
2024-10-21 12:17:14 +0300

    Update Portfolio CSS

--- Portfolio/Testing ground/script.js
+++ Portfolio/Testing ground/script.js
@@ -1,6 +1,24 @@
+const backToTop = this.document.querySelector("#backtopBtn");
+const navList = document.querySelector("nav ul")
+const mobButton = document.querySelector(".mobile")
+
+function toggleMenu() {
+    navList.classList.toggle("responsive")
+}
+mobButton.addEventListener("click",toggleMenu)
+
 window.addEventListener('scroll', function() {
     const header = document.querySelector("nav");
     
     header.classList.toggle('scrolled', window.scrollY > 50);
+    backToTop.classList.toggle("hidden",window.scrollY < 50);
 });
 
+backToTop.addEventListener("click",() => {
+    document.body.scrollTop= 0;
+    document.documentElement.scrollTop = 0;
+    
+    })
+
+
+console.log(backToTop)
